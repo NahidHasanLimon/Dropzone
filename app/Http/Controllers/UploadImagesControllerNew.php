e71f3ed175fed4a5af7ed0ed245907a152cc926c
nh.limon <nh.limon@yahoo.com>
2020-02-20 11:16:02 +0600

    submit dropzone with others input in a form

---
+++ app/Http/Controllers/UploadImagesControllerNew.php
@@ -0,0 +1,99 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Upload;
+use App\New_Upload;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Str;
+
+ 
+class UploadImagesControllerNew extends Controller
+{
+ 
+    private $photos_path;
+ 
+    public function __construct()
+    {
+        $this->photos_path = public_path('/images');
+    }
+ 
+    /**
+     * Display all of the images.
+     *
+     * @return \Illuminate\Http\Response
+     */
+   
+    public function index()
+    {
+        $photos_new = New_Upload::all();
+        return view('uploaded-images-new', compact('photos_new'));
+    }
+ 
+    /**
+     * Show the form for creating uploading new images.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+     public function create()
+    {
+        return view('upload_new');
+    }
+ 
+    /**
+     * Saving images uploaded through XHR Request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+   
+  public function store(Request $request)
+    {
+    	// dd($request->file('file'));
+
+        $photos = $request->file('file');
+        // $photos = $request->base64_decode(request('file'));
+ 
+        if (!is_array($photos)) {
+            $photos = [$photos];
+        }
+ 
+        if (!is_dir($this->photos_path)) {
+            mkdir($this->photos_path, 0777);
+        }
+ 
+        for ($i = 0; $i < count($photos); $i++) {
+            $photo = $photos[$i];
+            $name = sha1(date('YmdHis') . Str::random(30));
+            $save_name = $name . '.' . $photo->getClientOriginalExtension();
+            $resize_name = $name . Str::random(2) . '.' . $photo->getClientOriginalExtension();
+ 
+            Image::make($photo)
+                ->resize(250, null, function ($constraints) {
+                    $constraints->aspectRatio();
+                })
+                ->save($this->photos_path . '/' . $resize_name);
+ 
+            $photo->move($this->photos_path, $save_name);
+ 
+            $upload = new New_Upload();
+            $upload->filename = $save_name;
+            $upload->resized_name = $resize_name;
+            $upload->original_name = basename($photo->getClientOriginalName());
+            $upload->save();
+        }
+        return Response::json([
+            'message' => 'Image saved Successfully'
+        ], 200);
+    }
+ 
+    /**
+     * Remove the images from the storage.
+     *
+     * @param Request $request
+     */
+    
+}
